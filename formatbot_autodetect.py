8de48cd53c35df46f80513f88015254e8c9be8f1
nickerbocker <nickerbocker@tuta.io>
2019-02-12 20:58:23 -0500

    Consolidated code to one file. Makes it super easy to deploy on P.A.

--- formatbot_autodetect.py
+++ formatbot_autodetect.py
@@ -1,12 +1,37 @@
+import configparser
 import re
 from typing import Pattern
 
 import praw
 
-import templates
-import utils
+SUBREDDIT = 'learnpython'
 
-SUBREDDIT = 'testingground4bots'
+template_auto = (
+    "Hello u/{op}, I'm a bot that can assist you with code-formatting for reddit. "
+    'I have detected the following potential issue(s) with your submission:\n\n'
+    "{issues_str}\n\n"
+    "If I am correct then please follow "
+    "[these instructions](https://www.reddit.com/r/learnpython/wiki/faq#wiki_how_do_i_format_code.3F) "
+    "to fix your code formatting. Thanks!"
+)
+
+
+def get_reddit():
+    """
+    Get a praw.Reddit instance from config
+    :return: praw.Reddit instance
+    """
+    config = configparser.ConfigParser()
+    config.read("formatbot.cfg")
+    reddit_config = config['Reddit']
+    reddit = praw.Reddit(
+        client_id=reddit_config['client_id'],
+        client_secret=reddit_config['client_secret'],
+        username=reddit_config['username'],
+        password=reddit_config['password'],
+        user_agent='CodeFormatBot',
+    )
+    return reddit
 
 
 class Issue:
@@ -21,20 +46,19 @@ class Issue:
 
 
 class IssueMultiInline(Issue):
-    # two or more consecutive lines with inline code
+    # two or more consecutive lines with inline code (wrong format button)
     pattern = re.compile(r'(?:\s*?`.*?`\s*?[\n]+){2,}', re.MULTILINE)
     description = "Multiple consecutive lines have been found to contain inline formatting."
 
 
 class IssueCodeBlock(Issue):
-    # def found on left margin
     pattern = re.compile(r'''
         ^(?:                    #is on the left-hand margin (not four-spaces in)
         class\s.*?              #class detection
         |def\s.*?\(.*?\)        #function detection
         |for\s.*?in.*?          #for loop detection
         ):
-        ''', re.VERBOSE|re.MULTILINE)
+        ''', re.VERBOSE | re.MULTILINE)
     description = "Python code found in submission but not encapsulated in a code block."
 
 
@@ -45,22 +69,24 @@ issues = [
 
 
 def main():
-    reddit = utils.get_reddit()
+    reddit = get_reddit()
     subreddit = reddit.subreddit(SUBREDDIT)
     submission: praw.reddit.models.Submission
     me = reddit.user.me().name.lower()
-    for submission in subreddit.new():
+    for submission in subreddit.stream.submissions():
         text = submission.selftext
         op = submission.author.name
         issues_found = [x.description for x in issues if x.is_issue(text)]
         if not issues_found:
+            print(f"no issues with {op}'s post")
             continue
         for comment in submission.comments:
             if comment.author.name.lower() == me:
+                print(f"I've already commented on {op}'s post.")
                 break
         else:
             issues_str = '\n'.join(f'{i}. {des}' for i, des in enumerate(issues_found, 1))
-            my_comment = templates.auto.format(op=op, issues_str=issues_str)
+            my_comment = template_auto.format(op=op, issues_str=issues_str)
             submission.reply(my_comment)
             print('Replied to', op)
 
--- templates.py
+++ templates.py
@@ -12,7 +12,7 @@ auto = (
     "Hello u/{op}, I'm a bot that can assist you with code-formatting for reddit. "
     'I have detected the following potential issue(s) with your submission:\n\n'
     "{issues_str}\n\n"
-    "If I am correct then please kindly follow " 
+    "If I am correct then please follow " 
     "[these instructions](https://www.reddit.com/r/learnpython/wiki/faq#wiki_how_do_i_format_code.3F) "
     "to fix your code formatting. Thanks!"
 )
